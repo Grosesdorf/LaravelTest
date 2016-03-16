0133f3fb9a721bd967058a33cdf445c5ffb92ca6
support <E:\_MAIL>
2016-03-16 14:56:36 +0200

    Edited Task 02

---
+++ 02/newssite/database/migrations/news/2016_03_16_140122_create_news_table_add_field_author.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsTableAddFieldAuthor extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('news', function (Blueprint $table) {
+            $table->integer('id_author')->after('text');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('news', function (Blueprint $table) {
+            //
+        });
+    }
+  }
